b2c667180bbecdcfa2804102c164b5238905f87b
Jonathan Freed <jon.freed@gmail.com>
2018-11-12 15:37:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,24 +15,24 @@ attendees.each do |name|
   batch_badge_creator(name)
 end
 
-# room assigner
-# def assign_rooms(names, rooms)
-#   number = names.length
-#   counter = 0
-#   while counter < number
-#     if names.length > 0
-#       attendee1 = names.sample
-#       names.delete(attendee1)
-#       room1 = rooms.sample
-#       rooms.delete(room1)
-#     end
-#     puts "#{names.attendee1}, your room number is #{rooms.room1}".
-#     counter += 1
-# end
-# end
+room assigner
+def assign_rooms(names, rooms)
+  number = names.length
+  counter = 0
+  while counter < number
+    if names.length > 0
+      attendee1 = names.sample
+      names.delete(attendee1)
+      room1 = rooms.sample
+      rooms.delete(room1)
+    end
+    puts "#{names.attendee1}, your room number is #{rooms.room1}".
+    counter += 1
+end
+end
 
-# assign_rooms(attendees, rooms)
-# rooms = [1, 2, 3, 4, 5, 6, 7]
+assign_rooms(attendees)
+
 # attendees.each do |rooms|
 #   puts assign_rooms(attendees)
 # end

7e5cdb62540d7828587de84eb9ff4e9ee160b75e
Jonathan Freed <jon.freed@gmail.com>
2018-11-12 15:19:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@
 
 # array of conference attendees
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
+rooms = [1, 2, 3, 4, 5, 6, 7]
 # badge creator
 def batch_badge_creator(name)
   puts "Hi, my name is #{name}!"
@@ -13,9 +13,14 @@ attendees.each do |name|
 end
 
 # room assigner
-def assign_rooms(name)
+def assign_rooms(array)
+  number = array.length
   counter = 0
-  while counter < attendees.length
+  while counter < number
+    if array.length > 0
+      attendee1 = array.sample
+      array.delete(attendee1)
+      
     puts "#{name}! Your room number is" "#{counter}".
     counter += 1
   end

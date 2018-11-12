f9307fc2e56d7e4bfe16a70035a97db06a31874a
Jonathan Freed <jon.freed@gmail.com>
2018-11-12 15:48:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,7 @@ def assign_rooms(names, rooms)
       names.delete(attendee1)
       room1 = rooms.sample
       rooms.delete(room1)
-    puts "#{names.(attendee1)}, your room number is #{rooms.(room1)}."
+    puts "#{names.[attendee1]}, your room number is #{rooms.[room1]}."
     counter += 1
   end
 end
